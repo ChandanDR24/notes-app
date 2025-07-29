a581bb3edcaf5a95da58fb9910a9f537c681732f
Chandan D R <97674187+ChandanDR24@users.noreply.github.com>
2025-07-29 13:35:24 +0530

    Update authMiddleware.ts

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 
 export const authMiddleware = (req: any, res: Response, next: NextFunction) => {
+  console.log("Received auth header:", req.headers.authorization);
   const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) return res.status(401).json({ message: "Token missing" });
