a3e1463e65a908bcf4595a421680186e1fe9a131
Chandan D R <97674187+ChandanDR24@users.noreply.github.com>
2025-07-29 14:03:04 +0530

    Update note.routes.ts

--- backend/src/routes/note.routes.ts
+++ backend/src/routes/note.routes.ts
@@ -3,10 +3,9 @@ import { createNote, getNotes, deleteNote } from "../controllers/noteController"
 import { authMiddleware } from "../middleware/authMiddleware";
 
 const router = express.Router();
-router.use(authMiddleware);
 
-router.post("/", authMiddleware, createNote);
-router.get("/", authMiddleware, getNotes);
-router.delete("/:id", authMiddleware, deleteNote);
+router.post("/", createNote);
+router.get("/", getNotes);
+router.delete("/:id", deleteNote);
 
 export default router;
